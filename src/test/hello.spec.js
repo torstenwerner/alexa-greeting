5d149bceccb07720743162d852703e06b7f74afb
Torsten Werner <torsten.werner@westernacher.com>
2016-12-27 09:42:11 +0100

    add mocha test

---
+++ src/test/hello.spec.js
@@ -0,0 +1,121 @@
+'use strict'
+
+const AWS = require('aws-sdk');
+const assert = require('assert');
+
+function getBasePayload() {
+    return {
+        session: {
+            new: false,
+            sessionId: "amzn1.echo-api.session.[unique-value-here]",
+            attributes: {
+            },
+            user: {
+                userId: "amzn1.ask.account.[unique-value-here]"
+            },
+            application: {
+                applicationId: "amzn1.ask.skill.[unique-value-here]"
+            }
+        },
+        version: "1.0",
+        request: {
+            locale: "en-US",
+            timestamp: "2016-10-27T21:06:28Z",
+            type: "LaunchRequest",
+            requestId: "amzn1.echo-api.request.[unique-value-here]"
+        },
+        context: {
+            AudioPlayer: {
+                playerActivity: "IDLE"
+            },
+            System: {
+                device: {
+                    supportedInterfaces: {
+                        AudioPlayer: {}
+                    }
+                },
+                application: {
+                    applicationId: "amzn1.ask.skill.[unique-value-here]"
+                },
+                user: {
+                    userId: "amzn1.ask.account.[unique-value-here]"
+                }
+            }
+        }
+    }
+}
+
+const lambda = new AWS.Lambda({
+    region: 'eu-west-1'
+});
+
+function launchCheck(err, data) {
+    assert.ifError(err);
+    assert.equal(data.StatusCode, 200);
+    const payload = JSON.parse(data.Payload);
+    assert.equal(payload.response.outputSpeech.type, 'SSML');
+    const outputSsml = payload.response.outputSpeech.ssml;
+    assert(outputSsml.indexOf('Hochverehrter') >= 0);
+    assert.equal(payload.response.shouldEndSession, false);
+    assert(payload.sessionAttributes.asking);
+};
+
+describe('the LaunchRequest', function () {
+    this.timeout(10000);
+
+    it('should greet the user', function (done) {
+        const params = {
+            FunctionName: 'HalloGast',
+            Payload: JSON.stringify(getBasePayload())
+        };
+        lambda.invoke(params, function (err, data) {
+            launchCheck(err, data);
+            done();
+        });
+    });
+});
+
+describe('the HelloIntent', function () {
+    this.timeout(10000);
+
+    it('should greet the user on first call', function (done) {
+        const payload = getBasePayload();
+        payload.request.type = 'IntentRequest';
+        payload.request.intent = {
+            name: 'HelloIntent'
+        };
+        const params = {
+            FunctionName: 'HalloGast',
+            Payload: JSON.stringify(payload)
+        };
+        lambda.invoke(params, function (err, data) {
+            launchCheck(err, data);
+            done();
+        });
+    });
+
+    it('should say goodbye on second call', function (done) {
+        const payload = getBasePayload();
+        payload.session.new = false;
+        payload.session.attributes.asking = true;
+        payload.request.type = 'IntentRequest';
+        payload.request.intent = {
+            name: 'HelloIntent'
+        };
+        const params = {
+            FunctionName: 'HalloGast',
+            Payload: JSON.stringify(payload)
+        };
+        lambda.invoke(params, function (err, data) {
+            assert.ifError(err);
+            assert.equal(data.StatusCode, 200);
+            const payload = JSON.parse(data.Payload);
+            assert.equal(payload.response.outputSpeech.type, 'SSML');
+            const outputSsml = payload.response.outputSpeech.ssml;
+            assert(outputSsml.indexOf('ein andermal') >= 0);
+            assert.equal(payload.response.shouldEndSession, true);
+            done();
+        });
+    });
+
+});
--- src/test/runtest.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-aws lambda invoke --function-name HalloGast --payload file://$PWD/HelloIntentFirst.json --region eu-west-1 /tmp/out01.json
-aws lambda invoke --function-name HalloGast --payload file://$PWD/HelloIntentSecond.json --region eu-west-1 /tmp/out02.json
